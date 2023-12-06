6eb54914c344957647d1fead35a42a742995a39a
ChinmayeeJuturu <139365017+ChinmayeeJuturu@users.noreply.github.com>
2023-12-06 21:45:52 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,93 +1,82 @@
-  const todoList = () => {
+const todoList = () => {
   all = [];
   const add = (todoItem) => {
     all.push(todoItem);
   };
-  const markAsComplete = (index) => {
-    all[index].completed = true;
-  };
+const markAsComplete = (index) => {
+  all[index].completed = true;
+};
 
-  let i;
-  const today = new Date();
-  const todayDate = today.getDate();
-  const overdue = () => {
-    // Write the date check condition here and return the array
-    // of overdue items accordingly.
-    const over = [];
-    for (i = 0; i < all.length; i++) {
-      let date = new Date(all[i].dueDate);
-      if (date.getDate() === todayDate - 1) {
-        over.push(all[i]);
-      }
+let i;
+const today = new Date();
+const todayDate = today.getDate();
+const overdue = () => {
+  const over = [];
+  for (i = 0; i < all.length; i++) {
+    let date = new Date(all[i].dueDate);
+    if (date.getDate() === todayDate - 1) {
+      over.push(all[i]);
     }
-    return over;
-  };
-
-  const dueToday = () => {
-    // Write the date check condition here and return the array
-    // of todo items that are due today accordingly.
-    const due = [];
-    for (i = 0; i < all.length; i++) {
-      let date = new Date(all[i].dueDate);
-      if (date.getDate() === todayDate) {
-        due.push(all[i]);
-      }
+  }
+  return over;
+};
+const dueToday = () => {
+  const due = [];
+    
+  for (i = 0; i < all.length; i++) {
+    let date = new Date(all[i].dueDate);
+    if (date.getDate() === todayDate) {
+      due.push(all[i]);
     }
-    return due;
-  };
+  }
+  return due;
+};
 
-  const dueLater = () => {
-    // Write the date check condition here and return the array
-    // of todo items that are due later accordingly.
-    const later = [];
-    for (i = 0; i < all.length; i++) {
-      let date = new Date(all[i].dueDate);
-      if (date.getDate() === todayDate + 1) {
-        later.push(all[i]);
-      }
+const dueLater = () => {
+  const later = [];
+  for (i = 0; i < all.length; i++) {
+    let date = new Date(all[i].dueDate);
+    if (date.getDate() === todayDate + 1) {
+      later.push(all[i]);
     }
-    return later;
-  };
-
-  const toDisplayableList = (list) => {
-    // Format the To-Do list here, and return the output string
-    // as per the format given above.
-    const output = [];
-    for (i = 0; i < list.length; i++) {
-      let date = new Date(list[i].dueDate);
-      const fd = date.toISOString().split("T")[0];
-      if (date.getDate() === todayDate) {
-        if (list[i].completed == false) {
-          output.push([ ] ${list[i].title});
-        } else {
-          output.push([x] ${list[i].title});
-        }
+  }
+  return later;
+};
+const toDisplayableList = (list) => {
+  const output = [];
+  for (i = 0; i < list.length; i++) {
+    let date = new Date(list[i].dueDate);
+    const fd = date.toISOString().split("T")[0];
+    if (date.getDate() === todayDate) {
+      if (list[i].completed == false) {
+        output.push(`[ ] ${list[i].title}`);
       } else {
-        if (list[i].completed == false) {
-          output.push([ ] ${list[i].title} ${fd});
-        } else {
-          output.push(`[x]  ${list[i].title} ${fd} `);
-        }
+        output.push(`[x] ${list[i].title}`);
+      }
+    } else {
+      if (list[i].completed == false) {
+        output.push(`[ ] ${list[i].title} ${fd}`);
+      } else {
+        output.push(`[x]  ${list[i].title} ${fd} `);
       }
     }
-    return output.join("\n");
-  };
-
-  return {
-    all,
-    add,
-    markAsComplete,
-    overdue,
-    dueToday,
-    dueLater,
-    toDisplayableList,
-  };
+  }
+  return output.join("\n");
+};
+return {
+  all,
+  add,
+  markAsComplete,
+  overdue,
+  dueToday,
+  dueLater,
+  toDisplayableList,
+};
 };
 
 // ####################################### #
 // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
 // ####################################### #
-
 module.exports = todoList;
 const todos = todoList();
 
